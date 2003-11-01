34a80c3117e264172a4863e9ecfde39821a2854d
Tanaka Akira <akr@fsij.org>
2003-11-01 17:14:27 +0000

    Regexp.alt removed.

--- htree/regexp-util.rb
+++ htree/regexp-util.rb
@@ -1,11 +1,3 @@
-def Regexp.alt(*args)
-  if args.empty?
-    /(?!)/
-  else
-    Regexp.compile(args.map {|arg| Regexp === arg ? arg.to_s : Regexp.quote(arg) }.join('|'))
-  end
-end
-
 class Regexp
   def disable_capture
     re = ''
