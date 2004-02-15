b60ab57841b217e34ec646e541813d918adeb19a
Tanaka Akira <akr@fsij.org>
2004-02-15 11:07:56 +0000

    useless semicolon removed.

--- htree/nodehier.rb
+++ htree/nodehier.rb
@@ -1,24 +1,24 @@
 module HTree
-  class Name; include HTree; end
-  class Context; include HTree; end
+  class Name; include HTree end
+  class Context; include HTree end
 
-  module Node; include HTree; end
+  module Node; include HTree end
 
-  module Leaf; include HTree; end
-    class Text; include Leaf, Node; end
-    module Markup; include Leaf; end
-      class STag; include Markup; end
-      class ETag; include Markup; end
-        class BogusETag < ETag; include Node; end
-      class XMLDecl; include Markup, Node; end
-      class DocType; include Markup, Node; end
-      class ProcIns; include Markup, Node; end
-      class Comment; include Markup, Node; end
-  module Container; include HTree; end
-    class Doc; include Container, Node; end
-    class Elem; include Container, Node; end
+  module Leaf; include HTree end
+    class Text; include Leaf, Node end
+    module Markup; include Leaf end
+      class STag; include Markup end
+      class ETag; include Markup end
+        class BogusETag < ETag; include Node end
+      class XMLDecl; include Markup, Node end
+      class DocType; include Markup, Node end
+      class ProcIns; include Markup, Node end
+      class Comment; include Markup, Node end
+  module Container; include HTree end
+    class Doc; include Container, Node end
+    class Elem; include Container, Node end
 
-  class Loc; include HTree; end
+  class Loc; include HTree end
 
   class Error < StandardError; end
 end
