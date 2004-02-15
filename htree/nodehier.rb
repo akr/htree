de15b50948f28e90bd484f88ba7da0b003571478
Tanaka Akira <akr@fsij.org>
2004-02-15 02:29:34 +0000

    add HTree::Node module.

--- htree/nodehier.rb
+++ htree/nodehier.rb
@@ -2,19 +2,21 @@ module HTree
   class Name; include HTree; end
   class Context; include HTree; end
 
+  module Node; include HTree; end
+
   module Leaf; include HTree; end
-    class Text; include Leaf; end
+    class Text; include Leaf, Node; end
     module Markup; include Leaf; end
       class STag; include Markup; end
       class ETag; include Markup; end
-        class BogusETag < ETag; end
-      class XMLDecl; include Markup; end
-      class DocType; include Markup; end
-      class ProcIns; include Markup; end
-      class Comment; include Markup; end
+        class BogusETag < ETag; include Node; end
+      class XMLDecl; include Markup, Node; end
+      class DocType; include Markup, Node; end
+      class ProcIns; include Markup, Node; end
+      class Comment; include Markup, Node; end
   module Container; include HTree; end
-    class Doc; include Container; end
-    class Elem; include Container; end
+    class Doc; include Container, Node; end
+    class Elem; include Container, Node; end
 
   class Error < StandardError; end
 end
