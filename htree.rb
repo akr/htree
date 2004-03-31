cb3e9a612e52230f0e01cf2c2a11768d6264a9ec
Tanaka Akira <akr@fsij.org>
2004-03-31 22:21:51 +0000

    Add RDoc.

--- htree.rb
+++ htree.rb
@@ -1,3 +1,16 @@
+#
+# = htree.rb
+#
+# HTML document tree
+#
+# Author:: Tanaka Akira <akr@m17n.org>
+#
+# HTree provides following methods.
+#
+# - HTree.parse(input) -> HTree::Doc
+# - HTree.parse_xml(input) -> HTree::Doc
+#
+
 require 'htree/parse'
 require 'htree/extract_text'
 require 'htree/equality'
