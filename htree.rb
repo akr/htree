8e60af954f55490cc5285b61d01ca191e98027cc
Tanaka Akira <akr@fsij.org>
2004-03-31 22:30:23 +0000

    document update.

--- htree.rb
+++ htree.rb
@@ -1,9 +1,17 @@
 #
 # = htree.rb
 #
-# HTML document tree
+# HTML/XML document tree
 #
 # Author:: Tanaka Akira <akr@m17n.org>
+#
+# == Examples
+#
+# === Example 1: dump a document tree
+#
+#   % ruby -rhtree -e 'pp HTree.parse(ARGF)' html-file
+#
+# == Method summary
 #
 # HTree provides following methods.
 #
