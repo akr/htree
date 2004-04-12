af8f9ffe2a33f83c5bbc82a9cb941650849c531c
Tanaka Akira <akr@fsij.org>
2004-04-12 12:32:31 +0000

    add more tests for span.

--- test/test-template.rb
+++ test/test-template.rb
@@ -14,27 +14,35 @@ class TestText < Test::Unit::TestCase
 
   def test_text
     assert_xhtml('<e>1</e>', '<e _text=1>d</e>')
+    assert_xhtml('1', '<span _text=1>d</span>')
+    assert_xhtml('<span x="2">1</span>', '<span x=2 _text=1>d</span>')
   end
 
   def test_attr
     assert_xhtml('<e x="1">d</e>', '<e _attr_x=1>d</e>')
+    assert_xhtml('<span x="1">d</span>', '<span _attr_x=1>d</span>')
   end
 
   def test_if
     assert_xhtml('<e>d</e>', '<e _if=true>d</e>')
     assert_xhtml('', '<e _if=false>d</e>')
-    assert_xhtml('<f>dd</f>',
-      '<e _if=false _else=m>d</e><f _template=m>dd</f>')
+    assert_xhtml('<f>dd</f>', '<e _if=false _else=m>d</e><f _template=m>dd</f>')
+
+    assert_xhtml('d', '<span _if=true>d</span>')
   end
 
   def test_iter
     assert_xhtml('<o><i>1</i></o><o><i>2</i></o><o><i>3</i></o>',
       '<o _iter=[1,2,3].each//v><i _text=v /></o>')
+    assert_xhtml('<i>1</i><i>2</i><i>3</i>',
+      '<span _iter=[1,2,3].each//v><i _text=v /></span>')
   end
 
   def test_iter_content
     assert_xhtml('<o><i>1</i><i>2</i><i>3</i></o>',
       '<o _iter_content=[1,2,3].each//v><i _text=v /></o>')
+    assert_xhtml('<i>1</i><i>2</i><i>3</i>',
+      '<span _iter_content=[1,2,3].each//v><i _text=v /></span>')
   end
 
   def test_call
@@ -42,8 +50,5 @@ class TestText < Test::Unit::TestCase
       '<e _call=m(1) /><f _template=m(v) _text=v></f>')
   end
 
-  def test_span
-    assert_xhtml('1', '<span _text=1>d</span>')
-  end
 
 end
