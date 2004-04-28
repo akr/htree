950f7827e17497e8c9c64acfdc048add86bce1b7
Tanaka Akira <akr@fsij.org>
2004-04-28 18:45:07 +0000

    use parent argument of REXML constructors.

--- htree/rexml.rb
+++ htree/rexml.rb
@@ -27,25 +27,25 @@ module HTree
     # convert to REXML tree.
     def to_rexml
       require 'rexml/document'
-      to_rexml_internal(DefaultContext)
+      to_rexml_internal(nil, DefaultContext)
     end
   end
 
   # :stopdoc:
 
   class Doc
-    def to_rexml_internal(context)
+    def to_rexml_internal(parent, context)
+      raise ArgumentError, "parent must be nil" if parent != nil
       result = REXML::Document.new
       self.children.each {|c|
-        c = c.to_rexml_internal(context)
-        result << c if c
+        c.to_rexml_internal(result, context)
       }
       result
     end
   end
 
   class Elem
-    def to_rexml_internal(context)
+    def to_rexml_internal(parent, context)
       ename = self.element_name
       ns_decl = {}
       if context.namespace_uri(ename.namespace_prefix) != ename.namespace_uri
@@ -53,9 +53,9 @@ module HTree
       end
 
       if ename.namespace_prefix
-        result = REXML::Element.new("#{ename.namespace_prefix}:#{ename.local_name}")
+        result = REXML::Element.new("#{ename.namespace_prefix}:#{ename.local_name}", parent)
       else
-        result = REXML::Element.new(ename.local_name)
+        result = REXML::Element.new(ename.local_name, parent)
       end
 
       self.each_attribute {|aname, atext|
@@ -63,7 +63,7 @@ module HTree
           if context.namespace_uri(aname.namespace_prefix) != aname.namespace_uri
             ns_decl[aname.namespace_prefix] = aname.namespace_uri
           end
-          result.add_attribute("#{aname.namespace_prefix}:#{name.local_name}", atext.to_s)
+          result.add_attribute("#{aname.namespace_prefix}:#{aname.local_name}", atext.to_s)
         else
           result.add_attribute(aname.local_name, atext.to_s)
         end
@@ -79,45 +79,48 @@ module HTree
       context = context.subst_namespaces(ns_decl)
 
       self.children.each {|c|
-        c = c.to_rexml_internal(context)
-        result << c if c
+        c.to_rexml_internal(result, context)
       }
       result
     end
   end
 
   class Text
-    def to_rexml_internal(context)
-      REXML::Text.new(self.rcdata, true, nil, true)
+    def to_rexml_internal(parent, context)
+      REXML::Text.new(self.rcdata, true, parent, true)
     end
   end
 
   class XMLDecl
-    def to_rexml_internal(context)
-      REXML::XMLDecl.new(self.version, self.encoding, self.standalone)
+    def to_rexml_internal(parent, context)
+      r = REXML::XMLDecl.new(self.version, self.encoding, self.standalone)
+      parent << r if parent
+      r
     end
   end
 
   class DocType
-    def to_rexml_internal(context)
-      REXML::DocType.new(self.root_element_name, self.generate_content)
+    def to_rexml_internal(parent, context)
+      REXML::DocType.new([self.root_element_name, self.generate_content], parent)
     end
   end
 
   class ProcIns
-    def to_rexml_internal(context)
-      REXML::Instruction.new(self.target, self.content)
+    def to_rexml_internal(parent, context)
+      r = REXML::Instruction.new(self.target, self.content)
+      parent << r if parent
+      r
     end
   end
 
   class Comment
-    def to_rexml_internal(context)
-      REXML::Comment.new(self.content)
+    def to_rexml_internal(parent, context)
+      REXML::Comment.new(self.content, parent)
     end
   end
 
   class BogusETag
-    def to_rexml_internal(context)
+    def to_rexml_internal(parent, context)
       nil
     end
   end
---
+++ test/test-rexml.rb
@@ -0,0 +1,63 @@
+require 'test/unit'
+require 'htree/parse'
+require 'htree/rexml'
+require 'rexml/document'
+
+class TestREXML < Test::Unit::TestCase
+  def test_doc
+    r = HTree.parse('<root/>').to_rexml
+    assert_instance_of(REXML::Document, r)
+  end
+
+  def test_elem
+    r = HTree.parse('<root a="b"/>').to_rexml
+    assert_instance_of(REXML::Element, e = r.root)
+    assert_equal('root', e.name)
+    assert_equal('b', e.attribute('a').to_s)
+  end
+
+  def test_text
+    r = HTree.parse('<root>aaa</root>').to_rexml
+    assert_instance_of(REXML::Text, t = r.root.children[0])
+    assert_equal('aaa', t.to_s)
+  end
+
+  def test_xmldecl
+    s = '<?xml version="1.0"?>'
+    r = HTree.parse(s + '<root>aaa</root>').to_rexml
+    assert_instance_of(REXML::XMLDecl, x = r.children[0])
+    assert_equal('1.0', x.version)
+    assert_equal(nil, x.standalone)
+
+    assert_instance_of(REXML::XMLDecl, HTree.parse(s).children[0].to_rexml)
+  end
+
+  def test_doctype
+    s = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
+    r = HTree.parse(s + '<html><title>xxx</title></html>').to_rexml
+    assert_instance_of(REXML::DocType, d = r.children[0])
+    assert_equal('html', d.name)
+    assert_equal('PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"', d.external_id)
+
+    assert_instance_of(REXML::DocType, HTree.parse(s).children[0].to_rexml)
+  end
+
+  def test_procins
+    r = HTree.parse('<root><?xxx yyy?></root>').to_rexml
+    assert_instance_of(REXML::Instruction, i = r.root.children[0])
+    assert_equal('xxx', i.target)
+    assert_equal('yyy', i.content)
+
+    assert_instance_of(REXML::Instruction, HTree.parse('<?xxx yyy?>').children[0].to_rexml)
+  end
+
+  def test_comment
+    r = HTree.parse('<root><!-- zzz --></root>').to_rexml
+    assert_instance_of(REXML::Comment, c = r.root.children[0])
+    assert_equal(' zzz ', c.to_s)
+  end
+
+  def test_bogusetag
+    assert_equal(nil, HTree.parse('</e>').children[0].to_rexml)
+  end
+end
